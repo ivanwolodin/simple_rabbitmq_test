b8fd6ad475eb8a50ee1fe7f7f4a5be7affc9e0ad
Ivan <ivanwolodin@gmail.com>
2020-05-16 00:43:22 +0500

    Added watchdog module, changed dockerfile

--- python_serve/recieve.py
+++ python_serve/recieve.py
@@ -12,12 +12,16 @@ channel = connection.channel()
 channel.queue_declare(queue='hello')
 
 channel.basic_qos(prefetch_count=2)
+
+
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
 
-
 channel.basic_consume(
-    queue='hello', on_message_callback=callback, auto_ack=True)
+    queue='hello',
+    on_message_callback=callback,
+    auto_ack=True
+)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- python_serve/send.py
+++ python_serve/send.py
@@ -1,17 +1,1 @@
 #!/usr/bin/env python
-import pika
-
-credentials = pika.PlainCredentials('user', 'user')
-parameters = pika.ConnectionParameters('127.0.0.1', 5672, '/', credentials)
-connection = pika.BlockingConnection(parameters)
-#
-# connection = pika.BlockingConnection(
-#     pika.ConnectionParameters(host='localhost'))
-
-channel = connection.channel()
-
-channel.queue_declare(queue='hello')
-
-channel.basic_publish(exchange='', routing_key='hello', body='boobs!')
-print(" [x] Sent 'I am the best!'")
-connection.close()
